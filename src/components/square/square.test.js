6460569a73224a005a4bafd150223988cdfee1b3
Ben Wiedermann <bwiedermann@users.noreply.github.com>
2020-03-05 00:56:52 -0800

    Fix error

--- src/components/square/square.test.js
+++ src/components/square/square.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Squar from './square';
+import Square from './square';
 
 it("renders without crashing", () => {
     const div = document.createElement("div");
